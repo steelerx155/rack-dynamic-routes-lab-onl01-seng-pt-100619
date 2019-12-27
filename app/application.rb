f456821a601fee86475ef8802532e120e3dccab5
Randy Steele <steelerx155@gmail.com>
2019-12-27 15:03:06 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
        if item =@@items.find{|i| i.name == item_name}
-        resp.write "#{item}\n"
+         resp.write item.price
       else
         resp.status = 404
         resp.write "Route not found"
