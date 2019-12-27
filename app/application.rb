575b784624ec9558a964275fd0ffbe4d814c17bb
Randy Steele <steelerx155@gmail.com>
2019-12-27 14:54:28 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,14 @@
 class Application
- 
+
+  @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-    
-    if req.path=="/items/"
+
+    if req.path.match(/items/)
+      @@items.each do |item|
+        resp.write "#{item}\n"
+      end
+end      
